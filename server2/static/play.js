e57e004841bcf5efbdec301c7049794124e58774
Lenni <lennivh24@gmail.com>
2021-01-05 15:59:44 +0000

    moved to external js

---
+++ server2/static/play.js
@@ -0,0 +1,45 @@
+function sendSomething() {
+
+    if (!conn) {
+        console.log("Not yet connected");
+        return false;
+    }
+    conn.send("hello");
+    return false;
+};
+
+function join(id) {
+    if (!conn) {
+        console.log("Failed to connect");
+    }
+    conn.send(`join ${id}`);
+}
+
+var conn;
+var id;
+
+const url = window.location.search;
+const params = new URLSearchParams(url);
+id = [...params.keys()][0];
+console.log(id);
+
+if (id == undefined) {
+    window.location.href = "/";
+}
+
+if (window["WebSocket"]) {
+    conn = new WebSocket("ws://" + document.location.host + "/ws");
+    conn.onclose = function (evt) {
+        var item = document.createElement("div");
+        item.innerHTML = "<b>Connection closed.</b>";
+    };
+    conn.onmessage = function (evt) {
+        console.log("Server sent" + evt.data);
+    };
+    conn.onopen = function (evt) {
+        join(id);
+    };
+} else {
+    var item = document.createElement("div");
+    item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
+}
